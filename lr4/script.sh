c728eb147e3ff71354dd40ee3035685a17794591
Sergey <markusanon5999@gmail.com>
2021-05-18 10:21:27 -0400

    Files of lr4

---
+++ lr4/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo rm -R repository_git
+echo "Please enter the links of git repository"
+read links
+sudo git remote remove public
+sudo git remote add public $links
+git remote -v
+mkdir repository_git
+cd repository_git
+git init
+sudo cp -R /home/serg/lr3-git/projects/lr3/script.sh /home/serg/repository_git
+sudo cp -R /home/serg/lr4/ /home/serg/repository_git
+git checkout -b Labs
+git status
+git add script.sh
+git commit -m 'Files of lr3'
+git add lr4/script.sh
+git commit -m 'Files of lr4'
+sudo git push $links Labs
