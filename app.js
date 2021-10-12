0ef3216683d2093f58277d3f58806e08adf89eb5
serg1231-git <markusanon5999@gmail.com>
2021-10-12 16:39:29 +0300

    Frontend pr9

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-var number_one = document.getElementById('input_numberone').value;
-var number_two = document.getElementById('input_numbertwo').value;
-var result = document.getElementById('result');
-var container = document.querySelectorAll('div >input');
-container.forEach(function (element) {
-    element.addEventListener('click', function () {
-        if (element.id == 'add') {
-            var result_operation = Number(number_one) + Number(number_two);
-            result.value = (result_operation).toString();
+function filter(array,fn){
+    var listnew=[];
+    for (var i=0; i<array.length; i++)
+    {
+        if(!(fn(array[i])%2))
+        {
+            listnew.push(array[i]);
         }
-        else if (element.id == 'sub') {
-            result_operation = Number(number_one) - Number(number_two);
-            result.value = (result_operation).toString();
-        }
-        else if (element.id == 'multi') {
-            result_operation = Number(number_one) * Number(number_two);
-            result.value = (result_operation).toString();
-        }
-        else if (element.id == 'division' && ((Number(number_one) == 0) || Number(number_two) == 0)) {
-            result.innerHTML = 'На ноль делить нельзя';
-        }
-        else {
-            result.value = (Number(number_one) / Number(number_two)).toString();
-        }
-    });
+    }
+    return listnew;
+}
+function div(index) {
+    return index%2==1;   
+}
+function show(){
+    var video=document.querySelector('#videoConnect');
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+    {
+        navigator.mediaDevices.getUserMedia({video:true}).then(function getStream(stream){
+            video.srcObject=stream;
+            video.play();
+            console.log('Connection video successfully');
+        })
+        .catch(function error_connection(error){
+            console.log('Error connection');
+        })
+    }
+}
+var url_button=document.getElementById('button_url');
+url_button.addEventListener('click',function(){
+    var input_url=document.getElementById('url_connect');
+    window.location=input_url.value;
+    console.log('Connection successfully',input_url.value);
 });
+var up=document.getElementById('up_id');
+var down=document.getElementById('down_id');
+up.addEventListener('click',function(){
+    console.log('В истории',history.length+' страниц');
+    history.back();
+})
+down.addEventListener('click',function(){
+    history.forward();
+})
